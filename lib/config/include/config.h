41807c4606baebfecb4535c2982c3ae257a56a7f
Matthew Gibson <frogman85978@gmail.com>
2025-11-19 10:43:12 -0500

    Added a touch program in main

--- lib/config/include/config.h
+++ lib/config/include/config.h
@@ -1,7 +1,7 @@
 /* ====================  General Configuration  ==================== */
 
-#ifndef CELL_H
-#define CELL_H
+#ifndef GENERAL_H
+#define GENERAL_H
 
 #define CONFIG_TOUCH_EN  // Comment to disable Touch and use mechanical buttons instead
 
