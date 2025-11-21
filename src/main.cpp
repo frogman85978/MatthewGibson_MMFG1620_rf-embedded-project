84d9f5595665bd20f17452b1ce248f99264d88c0
Matthew Gibson <frogman85978@gmail.com>
2025-11-21 11:00:03 -0500

    Commented out touch interface in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <Arduino.h>
+
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <driver/gpio.h>
@@ -32,77 +34,85 @@ static void IRAM_ATTR touch_isr(void * arg) {
         }
     }
 }
+void setup(void) {
+    // Install the IO interrupt service
+    gpio_install_isr_service(0);
+}
 
-extern "C" void app_main() {
+void loop(void) {
+
+}
+
+// extern "C" void app_main() {
     /* --------------------  Setup Display  --------------------*/
     /* --------------------  Setup Sensors  --------------------*/
     /* --------------------  Setup LEDs  --------------------*/
 
     // gpio_set_direction((gpio_num_t)IO_LED_1, GPIO_MODE_OUTPUT);
-    gpio_set_direction((gpio_num_t)IO_TOUCH_1, GPIO_MODE_INPUT);
-    gpio_set_direction((gpio_num_t)IO_TOUCH_2, GPIO_MODE_INPUT);
-    gpio_set_direction((gpio_num_t)IO_TOUCH_3, GPIO_MODE_INPUT);
+    // gpio_set_direction((gpio_num_t)IO_BUTTON_1, GPIO_MODE_INPUT);
+    // gpio_set_direction((gpio_num_t)IO_BUTTON_2, GPIO_MODE_INPUT);
+    // gpio_set_direction((gpio_num_t)IO_BUTTON_3, GPIO_MODE_INPUT);
 
-    ESP_ERROR_CHECK(touch_pad_init());
-    touch_pad_set_fsm_mode(TOUCH_FSM_MODE_TIMER);
-    touch_pad_set_voltage(TOUCH_HVOLT_MAX, TOUCH_LVOLT_MAX, TOUCH_HVOLT_ATTEN_MAX);
-    ESP_ERROR_CHECK(touch_pad_config(TOUCH_PAD_NUM5, 0));
-    ESP_ERROR_CHECK(touch_pad_config(TOUCH_PAD_NUM6, 0));
-    ESP_ERROR_CHECK(touch_pad_config(TOUCH_PAD_NUM7, 0));
+    // ESP_ERROR_CHECK(touch_pad_init());
+    // touch_pad_set_fsm_mode(TOUCH_FSM_MODE_TIMER);
+    // touch_pad_set_voltage(TOUCH_HVOLT_MAX, TOUCH_LVOLT_MAX, TOUCH_HVOLT_ATTEN_MAX);
+    // ESP_ERROR_CHECK(touch_pad_config(TOUCH_PAD_NUM5, 0));
+    // ESP_ERROR_CHECK(touch_pad_config(TOUCH_PAD_NUM6, 0));
+    // ESP_ERROR_CHECK(touch_pad_config(TOUCH_PAD_NUM7, 0));
 
-    touch_pad_filter_start(10);
+    // touch_pad_filter_start(10);
 
-    touch_pad_t touch_pads[3] = {
-        TOUCH_PAD_NUM5,
-        TOUCH_PAD_NUM6,
-        TOUCH_PAD_NUM7
-    };
+    // touch_pad_t touch_pads[3] = {
+    //     TOUCH_PAD_NUM5,
+    //     TOUCH_PAD_NUM6,
+    //     TOUCH_PAD_NUM7
+    // };
 
-    // Calibrate pads
-    uint16_t base;
-    for (int i = 0; i < 3; i++) {
-        touch_pad_read_filtered(touch_pads[i], &base);
-        uint32_t thresh = base * 0.8;
-        touch_pad_set_thresh(touch_pads[i], thresh);
-        printf("Pad %d base=%d threshold=%lu\n", i, base, thresh);
-    }
+    // // Calibrate pads
+    // uint16_t base;
+    // for (int i = 0; i < 3; i++) {
+    //     touch_pad_read_filtered(touch_pads[i], &base);
+    //     uint32_t thresh = base * 0.8;
+    //     touch_pad_set_thresh(touch_pads[i], thresh);
+    //     printf("Pad %d base=%d threshold=%lu\n", i, base, thresh);
+    // }
 
-    touch_pad_isr_register(touch_isr, NULL);
-    touch_pad_intr_enable();
+    // touch_pad_isr_register(touch_isr, NULL);
+    // touch_pad_intr_enable();
 
-    ESP_ERROR_CHECK(touch_pad_filter_start(10));
+    //     ESP_ERROR_CHECK(touch_pad_filter_start(10));
 
 
 
-    for (;;) {
-        if (t1) {
-            t1 = false;
-            printf("t1 pressed\n");
-        }
+    //     for (;;) {
+    //         if (t1) {
+    //             t1 = false;
+    //             printf("t1 pressed\n");
+    //         }
 
-        if (t2) {
-            t2 = false;
-            printf("t2 pressed\n");
-        }
+    //         if (t2) {
+    //             t2 = false;
+    //             printf("t2 pressed\n");
+    //         }
 
-        if (t3) {
-            t3 = false;
-            printf("t3 pressed\n");
-        }
+    //         if (t3) {
+    //             t3 = false;
+    //             printf("t3 pressed\n");
+    //         }
 
-        vTaskDelay(100 / portTICK_PERIOD_MS);
-        // uint16_t touch_value_1 = 0;
-        // uint16_t touch_value_2 = 0;
-        // uint16_t touch_value_3 = 0;
-        // touch_pad_read_filtered(TOUCH_PAD_NUM5, &touch_value_1);
-        // touch_pad_read_filtered(TOUCH_PAD_NUM6, &touch_value_2);
-        // touch_pad_read_filtered(TOUCH_PAD_NUM7, &touch_value_3);
-        // printf("%d, %d, %d\r", touch_value_1, touch_value_2, touch_value_3);
-        // ESP_LOGI("MAIN", "%d", touch_value);
+    //         vTaskDelay(100 / portTICK_PERIOD_MS);
+    //         // uint16_t touch_value_1 = 0;
+    //         // uint16_t touch_value_2 = 0;
+    //         // uint16_t touch_value_3 = 0;
+    //         // touch_pad_read_filtered(TOUCH_PAD_NUM5, &touch_value_1);
+    //         // touch_pad_read_filtered(TOUCH_PAD_NUM6, &touch_value_2);
+    //         // touch_pad_read_filtered(TOUCH_PAD_NUM7, &touch_value_3);
+    //         // printf("%d, %d, %d\r", touch_value_1, touch_value_2, touch_value_3);
+    //         // ESP_LOGI("MAIN", "%d", touch_value);
 
-        // gpio_set_level((gpio_num_t)IO_LED_1, 1);
-        // vTaskDelay(500 / portTICK_PERIOD_MS);
-        // gpio_set_level((gpio_num_t)IO_LED_1, 0);
-        // vTaskDelay(500 / portTICK_PERIOD_MS);
-    }
-}
+    //         // gpio_set_level((gpio_num_t)IO_LED_1, 1);
+    //         // vTaskDelay(500 / portTICK_PERIOD_MS);
+    //         // gpio_set_level((gpio_num_t)IO_LED_1, 0);
+    //         // vTaskDelay(500 / portTICK_PERIOD_MS);
+    //     }
+    // }
